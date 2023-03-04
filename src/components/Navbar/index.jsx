ec898193006255012a1afa5b64caadddb355ce02
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2023-03-03 23:42:31 -0300

    Implementado state de Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,18 +1,27 @@
+import { useState } from 'react';
 import navStyle from './Navbar.module.css'
 
 
-function Navbar() {
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+    function toggleButton() {
+        setOpen(!open);
+    }
+
     return (
         <nav className={navStyle.navbar}>
             <div className={navStyle.logo}>Logo</div>
-            <ul>
+            <ul className={`${open ? navStyle.show : ''}`}>
                 <li><a href="#home">Home</a></li>
                 <li><a href="#services">Services</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
 
             <div className={navStyle.responsive_navbar}>
-                <button className={navStyle.toggle_btn} aria-label="Abrir menu">
+                <button className={`${navStyle.toggle_btn} ${open ? navStyle.open : ''}`} onClick={toggleButton}
+                    aria-label="Abrir menu"
+                    aria-expanded={open ? 'true' : 'false'}>
                     <span></span>
                     <span></span>
                     <span></span>
