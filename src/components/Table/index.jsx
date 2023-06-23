66b937f5bd6477d2765f6947dc39a13fcdbccce5
Cleyton Ferreira <cleytonferreira.dev@gmail.com>
2023-06-23 11:25:43 -0300

    Adicionada linha de totais em Table

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import products from './products';
 import tableStyle from './Table.module.css';
 
@@ -5,17 +6,29 @@ const formatPrice = (price) => {
     const formattedPrice = price.toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL',
-        minimunFractionDigits: 2
+        minimumFractionDigits: 2
     });
 
     return formattedPrice;
 }
 
 const Table = () => {
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        const prices = products.map(product => product.price);
+        const totalPrice = prices.reduce((sum, price) => {
+            return sum + price;
+        });
+
+        setTotal(totalPrice);
+    }, []);
+
     const productsTable = products.map(product => {
         return (
             <tr key={product.id}>
-                <td>{`${product.id}`}</td><td>{`${product.name}`}</td>
+                <td>{`${product.id}`}</td>
+                <td>{`${product.name}`}</td>
                 <td>{formatPrice(product.price)}</td>
             </tr>
         )
@@ -32,10 +45,13 @@ const Table = () => {
             </thead>
             <tbody>
                 {productsTable}
+                <tr>
+                    <td colSpan="2"><b>Total</b></td>
+                    <td><b>{formatPrice(total)}</b></td>
+                </tr>
             </tbody>
         </table>
     )
-
 };
 
 export default Table;
